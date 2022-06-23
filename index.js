889e99251dde56e30be4d6e864da9b1b51986bdb
praveen6289 <kuthadipraveen366@gmail.comgit config --global user.name praveen6289>
2022-06-23 13:38:45 +0000

    Solution Social Buttons

---
+++ index.js
@@ -0,0 +1,20 @@
+const Button = (props) => {
+  //  Write your code here.
+  const { buttonText, className } = props;
+  return <button className={className}>{buttonText}</button>;
+};
+
+const element = (
+  <div className="bg-container">
+    <div className="social-media-container">
+      <h1 className="heading">Social Buttons</h1>
+      <div className="button-container">
+        <Button buttonText="Like" className="like-button" />
+        <Button buttonText="Comment" className="comment-button" />
+        <Button buttonText="Share" className="share-button" />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
